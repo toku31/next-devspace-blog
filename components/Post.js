8f5a503cb8c39d644c33c2bb8083d662f7ffd792
garden627 <garden627@yahoo.co.jp>
2022-03-06 10:08:03 +0900

    Pagination & Category Pages

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import CategoryLabel from './CategoryLabel';
 
 export default function Post({post}) {
-  console.log('catagory', post.category)
+  // console.log('Post.js category', post.category)
   return (
   <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
     <Image 
